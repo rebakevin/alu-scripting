284365ba5dfc2b90ffc62fa55eee50791ee51d3c
rebakevin <r.kevin@alustudent.com>
2025-11-14 09:03:18 +0000

    fix

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,28 @@
 #!/usr/bin/python3
-"""Script that fetch 10 hot post for a given subreddit."""
+"""Script that prints the titles of the first 10 hot posts."""
 import requests
 
 
 def top_ten(subreddit):
-    """Return number of subscribers if @subreddit is valid subreddit.
-    if not return 0."""
+    """Prints the titles of the first 10 hot posts for a subreddit.
+    If invalid, prints None.
+    """
 
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers)
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "MyAPI/0.0.1"}
 
-    if response.status_code == 200:
-        json_data = response.json()
-        for i in range(10):
-            print(
-                json_data.get('data')
-                .get('children')[i]
-                .get('data')
-                .get('title')
-            )
-    else:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
         print(None)
+        return
+
+    data = response.json().get("data", {}).get("children", [])
+
+    if not data:
+        print(None)
+        return
+
+    for post in data:
+        print(post.get("data", {}).get("title"))
+
